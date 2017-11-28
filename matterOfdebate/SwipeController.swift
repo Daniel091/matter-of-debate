f78779926ad02cb7809167d6bad39301ca8d92aa
simplyGregory <31621667+einfachgreg@users.noreply.github.com>
2017-11-28 23:03:08 +0100

    Added UI Elements for Swipe UI in the favourites Tab

---
+++ matterOfdebate/SwipeController.swift
@@ -0,0 +1,16 @@
+//
+//  SwipeController.swift
+//  matterOfdebate
+//
+//  Created by Gregor Anzer on 28.11.17.
+//  Copyright © 2017 Gruppe7. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SwipeController:UISwipeGestureRecognizer {
+    
+    
+    
+}
