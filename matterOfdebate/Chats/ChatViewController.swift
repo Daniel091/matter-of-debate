15f1847159e4076ab570ed9e6262bef9786ecd1f
daniel_eichinger <d.eichinger@campus.lmu.de>
2017-11-29 13:20:40 +0100

    first visible demo of jsqmessage controller

---
+++ matterOfdebate/Chats/ChatViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ChatViewController.swift
+//  matterOfdebate
+//
+//  Created by Daniel Eichinger on 29.11.17.
+//  Copyright © 2017 Gruppe7. All rights reserved.
+//
+
+import UIKit
+import JSQMessagesViewController
+
+class ChatViewController: JSQMessagesViewController {
+    // array that stores messages
+    var messages = [JSQMessage]()
+    
+    // create colored message bubbles, outgoing is blue, incoming gray
+    // lazy vars are only initialized once, when there accessed
+    lazy var outgoingBubble: JSQMessagesBubbleImage = {
+        return JSQMessagesBubbleImageFactory()!.outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
+    }()
+    
+    lazy var incomingBubble: JSQMessagesBubbleImage = {
+        return JSQMessagesBubbleImageFactory()!.incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleLightGray())
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        senderId = "1234"
+        senderDisplayName = "..."
+        print("Yay")
+        
+        // hide attachement button
+        inputToolbar.contentView.leftBarButtonItem = nil
+        
+        // hidding avatar
+        collectionView.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
+        collectionView.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
+    }
+    
+    // returns message from specific index
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    // returns total number of messages
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    // gets the right bubble image for each kind of message (incoming, outgoing)
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        return messages[indexPath.item].senderId == senderId ? outgoingBubble : incomingBubble
+    }
+
+    // hiding of avatars by now
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    // label for message bubble
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAt indexPath: IndexPath!) -> NSAttributedString! {
+        return messages[indexPath.item].senderId == senderId ? nil : NSAttributedString(string: messages[indexPath.item].senderDisplayName)
+    }
+    
+    // sets height of label
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAt indexPath: IndexPath!) -> CGFloat {
+        return messages[indexPath.item].senderId == senderId ? 0 : 15
+    }
+    
+}
