1e08c7b0bdc3f1fb7b8e4c934609ae632d32b77a
daniel_eichinger <d.eichinger@campus.lmu.de>
2017-12-10 14:09:17 +0100

    Table View now only shows chats of logged in user

--- matterOfdebate/Chats/UserChatsTableViewController.swift
+++ matterOfdebate/Chats/UserChatsTableViewController.swift
@@ -9,11 +9,12 @@
 import Firebase
 class UserChatsTableViewController: UITableViewController {
 
+    private let dateFormatter = DateFormatter()
     private var chats: [Chat] = []
+    
     private lazy var chatsRef = Constants.refs.databaseChats
     private var chatsRefHandle: DatabaseHandle?
     private var chatsRefUpdateHandle: DatabaseHandle?
-    private let dateFormatter = DateFormatter()
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -39,8 +40,15 @@ class UserChatsTableViewController: UITableViewController {
     }
     
     private func observeChats() {
+        // gets user
+        let user_obj = SingletonUser.sharedInstance.user
+        
         // Childs are added
-        chatsRefHandle = chatsRef.observe(.childAdded, with: { (snapshot) -> Void in
+        chatsRefHandle = chatsRef
+            .queryOrdered(byChild: "users/" + user_obj.uid)
+            .queryEqual(toValue: true)
+            .observe(.childAdded, with: { (snapshot) -> Void in
+                
             let chatsData = snapshot.value as! Dictionary<String, AnyObject>
             
             // if title is set construct new Chat Object
@@ -57,7 +65,11 @@ class UserChatsTableViewController: UITableViewController {
         })
         
         // Childs are changed
-        chatsRefUpdateHandle = chatsRef.observe(.childChanged, with: { (snapshot) -> Void in
+        chatsRefUpdateHandle = chatsRef
+                .queryOrdered(byChild: "users/" + user_obj.uid)
+                .queryEqual(toValue: true)
+                .observe(.childChanged, with: { (snapshot) -> Void in
+                    
             let chatsData = snapshot.value as! Dictionary<String, AnyObject>
             
             // if title is set construct new Chat Object
@@ -123,50 +135,4 @@ class UserChatsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return chats.count
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
