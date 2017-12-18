8348432d35164c6b74799c09d9bac4f0ba07aaec
Gregor Anzer <gregor.anzer@gmail.com>
2017-12-18 04:37:22 +0100

    pan gesture added

--- matterOfdebate/SwipeViewController.swift
+++ matterOfdebate/SwipeViewController.swift
@@ -15,9 +15,9 @@ class SwipeViewController: UIViewController {
     @IBOutlet weak var imgSwipe: UIImageView!
     @IBOutlet weak var labelSwipe: UILabel!
     @IBOutlet weak var swipeText: UITextField!
-    @IBOutlet weak var swipeYes: UIImageView!
-    @IBOutlet weak var swipeNo: UIImageView!
-    
+    @IBOutlet weak var swipeNo: UIButton!
+    @IBOutlet weak var swipeYes: UIButton!
+    @IBOutlet var topView: UIView!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -25,6 +25,7 @@ class SwipeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
     @IBAction func handleTapNo(_ sender: UITapGestureRecognizer) {
@@ -36,6 +37,30 @@ class SwipeViewController: UIViewController {
     @IBAction func handleTapYes(_ sender: UITapGestureRecognizer){
         if(sender.state == UIGestureRecognizerState.ended) {
             print("you tapped yes");
+        }
+    }
+    @IBAction func handleSwipe(_ sender: UIPanGestureRecognizer)
+    {
+        var translation = sender.translation(in: self.view)
+        
+        switch (sender.state) {
+        case UIGestureRecognizerState.began:
+            break
+        case .possible:
+            break
+        case .changed:
+            let translation = sender.translation(in: self.view)
+            if let view = sender.view {
+                view.center = CGPoint(x:view.center.x + translation.x,
+                                      y:view.center.y + translation.y)
+            }
+            sender.setTranslation(CGPoint.zero, in: self.view)
+        case .ended:
+            break
+        case .cancelled:
+           break
+        case .failed:
+            break
         }
     }
 }
