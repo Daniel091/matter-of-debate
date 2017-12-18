f60ecea267ed6824504858945656f5d6df9a7517
Gregor Anzer <gregor.anzer@gmail.com>
2017-12-18 03:31:18 +0100

    UI updated All UI elements for the view added Taps for yes and no work

--- matterOfdebate/SwipeViewController.swift
+++ matterOfdebate/SwipeViewController.swift
@@ -2,8 +2,40 @@
 //  SwipeViewController.swift
 //  matterOfdebate
 //
-//  Created by Gregor Anzer on 05.12.17.
+//  Created by Gregor Anzer on 18.12.17.
 //  Copyright © 2017 Gruppe7. All rights reserved.
 //
 
 import Foundation
+import UIKit
+
+class SwipeViewController: UIViewController {
+    
+    @IBOutlet weak var swipeContainer: UIView!
+    @IBOutlet weak var imgSwipe: UIImageView!
+    @IBOutlet weak var labelSwipe: UILabel!
+    @IBOutlet weak var swipeText: UITextField!
+    @IBOutlet weak var swipeYes: UIImageView!
+    @IBOutlet weak var swipeNo: UIImageView!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func handleTapNo(_ sender: UITapGestureRecognizer) {
+        if(sender.state == UIGestureRecognizerState.ended)
+        {
+            print("you tapped no")
+        }
+    }
+    @IBAction func handleTapYes(_ sender: UITapGestureRecognizer){
+        if(sender.state == UIGestureRecognizerState.ended) {
+            print("you tapped yes");
+        }
+    }
+}
