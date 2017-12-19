f108b5472be0b041776159817e05f894152b5409
daniel_eichinger <d.eichinger@campus.lmu.de>
2017-12-19 16:44:58 +0100

    settings now work with eureka

--- matterOfdebate/SettingsController.swift
+++ matterOfdebate/SettingsController.swift
@@ -8,15 +8,73 @@
 
 import UIKit
 import Firebase
+import Eureka
 
-class SettingsController: UIViewController {
+class SettingsController: FormViewController {
+    let user_obj = SingletonUser.sharedInstance.user
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if !user_obj.isAnonymous {
+            form +++ Section("Benutzer Informationen")
+                <<< TextRow("username"){ row in
+                    row.title = "Username"
+                    row.value = self.user_obj.user_name
+                }
+                <<< TextRow("email"){
+                    $0.title = "Email"
+                    $0.value = self.user_obj.email
+                }
+                <<< ButtonRow("save"){
+                    $0.title = "Save"
+                    }.onCellSelection({ (cell, row) in
+                        self.saveUsrData()
+                    })
+        }
+        
+        form +++ Section("Weitere Einstellungen")
+            <<< ButtonRow("logout"){
+                $0.title = "Logout"
+                }.onCellSelection({ (cell, row) in
+                    self.logoutUser()
+                })
+        
+        if user_obj.isAdmin && !user_obj.isAnonymous{
+            form +++ Section("Admin-Panel")
+                <<< ButtonRow("create Theme"){
+                    $0.title = "Create new Theme"
+                    }.onCellSelection({ (cell, row) in
+                        self.triggerCreateTheme()
+                    })
+        }
+        
     }
+    
+    func saveUsrData() {
+        print("Trying to save usr data")
+        let valuesDictionary = form.values()
 
- 
-    @IBAction func logoutUser(_ sender: UIButton) {
+        guard var email = valuesDictionary["email"]!, var usr_name = valuesDictionary["username"]! else {
+            return
+        }
+        
+        // cast image and titel
+        email = email as! String
+        usr_name = usr_name as! String
+        
+        let ref_usr = Constants.refs.databaseUsers.child(self.user_obj.uid)
+        let childUpdates = ["email": email,
+                            "username": usr_name] as [String : Any]
+        
+        // TODO Update singleton
+        
+        ref_usr.updateChildValues(childUpdates)
+        
+        
+    }
+    
+    func logoutUser() {
         print(":-) trying to logout")
         do {
             try Auth.auth().signOut()
@@ -31,7 +89,7 @@ class SettingsController: UIViewController {
         
     }
     
-    @IBAction func triggeredAdminCreateTheme(_ sender: UIButton) {
+    func triggerCreateTheme() {
         self.performSegue(withIdentifier: "adminThemeControlSeque", sender: self)
     }
 
