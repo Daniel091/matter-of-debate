04dca41b96085b92960f0263373a0289bf001a9a
Benedikt Winter <b.winter1992@gmail.com>
2017-12-20 11:23:04 +0100

    Überprüfen, ob der neue Username bzw die neue emailbereits in der Datenbank existieren. Wenn nicht, update Datenbank.

--- matterOfdebate/SettingsController.swift
+++ matterOfdebate/SettingsController.swift
@@ -11,6 +11,7 @@ import Firebase
 import Eureka
 
 class SettingsController: FormViewController {
+    
     let user_obj = SingletonUser.sharedInstance.user
 
     override func viewDidLoad() {
@@ -51,27 +52,56 @@ class SettingsController: FormViewController {
         
     }
     
+    // Checks in Firebase for douplicates and then updates the user data
     func saveUsrData() {
         print("Trying to save usr data")
         let valuesDictionary = form.values()
 
-        guard var email = valuesDictionary["email"]!, var usr_name = valuesDictionary["username"]! else {
+        guard let email = valuesDictionary["email"]!, let usr_name = valuesDictionary["username"]! else {
             return
         }
         
-        // cast image and titel
-        email = email as! String
-        usr_name = usr_name as! String
+        // test if new username is already taken
+        var ref: DatabaseReference!
+        ref = Database.database().reference().child("users")
+        ref.queryOrdered(byChild: "username").queryEqual(toValue: usr_name as! String).observeSingleEvent(of: .value) { (userSnapshot) in
+
+            if userSnapshot.childrenCount != 0 {
+                print("Username already exists")
+                
+                // TODO: benachrichtige den user.
+                
+            } else {
+
+                // update Username
+                let ref_usr = Constants.refs.databaseUsers.child(self.user_obj.uid)
+                let childUpdates = ["username": usr_name] as [String : Any]
+
+                ref_usr.updateChildValues(childUpdates)
+                print("username changed")
+            }
+        }
         
-        let ref_usr = Constants.refs.databaseUsers.child(self.user_obj.uid)
-        let childUpdates = ["email": email,
-                            "username": usr_name] as [String : Any]
-        
-        // TODO Update singleton
-        
-        ref_usr.updateChildValues(childUpdates)
-        
-        
+        // test if email already exists
+        ref.queryOrdered(byChild: "email").queryEqual(toValue: email as! String).observeSingleEvent(of: .value) { (userSnapshot) in
+            
+            if userSnapshot.childrenCount != 0 {
+                print("email already exists")
+                
+                // TODO: benachrichtige den user.
+                
+            } else {
+                
+                // update email
+                let ref_email = Constants.refs.databaseUsers.child(self.user_obj.uid)
+                let childUpdates = ["email": email] as [String : Any]
+                
+                // TODO: Update singleton
+                
+                ref_email.updateChildValues(childUpdates)
+                print("email changed")
+            }
+        }
     }
     
     func logoutUser() {
